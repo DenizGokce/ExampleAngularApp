75d4cf2cc3c5f36134e744d77ff45bfc2737dee7
Deniz Gökçe <denizgokce93@gmail.com>
2017-11-20 02:27:32 +0200

    Package.json update!

--- server.js
+++ server.js
@@ -25,7 +25,24 @@ app.use(function(request, response, next){
 });
 app.use(express.static(__dirname+"/public"));
 
+const forceSSL = function() {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+
+// ...
+app.use(forceSSL());
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Kibana server listening on port ' + app.get('port'));
 });
+
