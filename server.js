38ad1e8bf7cdfceba84e0482047fa83ae33c5f12
Deniz Gökçe <denizgokce93@gmail.com>
2017-11-20 03:06:42 +0200

    Package.json update!

--- server.js
+++ server.js
@@ -1,48 +1,9 @@
-/**
- * Module dependencies.
- */
 var express = require('express');
-var http = require('http');
-
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 8000);
+app.use(express.static(__dirname + '/app'));
 
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+// use port 3000 unless there exists a preconfigured port
+var port = process.env.port || 5000;
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
-
-app.use(function(request, response, next){
-    //this intercepts all requests
-    //usefull for logging and stuff
-    next();
-});
-app.use(express.static(__dirname+"/public"));
-
-const forceSSL = function() {
-    return function (req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect(
-                ['https://', req.get('Host'), req.url].join('')
-            );
-        }
-        next();
-    }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-
-// ...
-app.use(forceSSL());
-
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Kibana server listening on port ' + app.get('port'));
-});
-
+app.listen(port);
