922c95283f14db8652c41cbe1319f1efc2120f11
denizgokce <denizgokce93@gmail.com>
2017-12-05 17:48:22 +0300

    Index.html update

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -1,6 +1,5 @@
 'use strict';
 angular.module('app.home', ['ngRoute', 'app.services'])
-
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/Home', {
             templateUrl: 'views/home/home.html',
@@ -8,12 +7,17 @@ angular.module('app.home', ['ngRoute', 'app.services'])
         });
     }])
     .controller('homeController', function ($rootScope, $scope, $location, config, peopleApi) {
+        $scope.loader = "loader-active";
+        $scope.loader_view = "loader-inactive";
         getPeople();
 
         function getPeople() {
             peopleApi.getPeople().then(function (people) {
+                $scope.loader = "loader-inactive";
                 $scope.people = people.data;
+                $scope.loader_view = "loader-active";
             }).catch(function (error) {
+                $scope.loader = "loader-inactive";
                 $scope.status = 'Unable to load people data: ' + error.message;
             });
         }
